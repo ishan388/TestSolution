469f1f3d7e89025b0cc98bb8909bce15c0d593ad
Ishan Malik <ishan388@gmail.com>
2022-07-14 01:02:54 +0100

    BlTests complete

--- NC_BLRepositories.Tests/BLProductsRepoTests.cs
+++ NC_BLRepositories.Tests/BLProductsRepoTests.cs
@@ -15,30 +15,36 @@ namespace NC_BLRepositories.Tests
 {
     public class BLProductsRepoTests
     {
-        private IServiceProvider ServicesProvider { get; set; }
-        private Mock<IDLProductsRepo> mockDLRepo = new Mock<IDLProductsRepo>();
         private BLProductsRepo blRepo;
+        private Mock<IDLProductsRepo> mockDLRepo = new Mock<IDLProductsRepo>();
+        ProductVM? product = new ProductVM
+        {
+            Name = "New Product",
+            Price = 100,
+            Status = (int)StatusEnm.Active,
+            StatusName = "Active"
+        };
 
         public BLProductsRepoTests()
         {
-            ServicesProvider = DependencySetupFixture.ConfigureDependencies();
+            blRepo = new BLProductsRepo(mockDLRepo.Object);
         }
 
         [Test]
         public void AddProduct_Test()
         {
-            int expected = 1003;
-            mockDLRepo.Setup(svc => svc.AddProduct(It.IsAny<ProductVM>())).ReturnsAsync(expected);
-            blRepo = new BLProductsRepo(mockDLRepo.Object);
-            int result = blRepo.AddProduct(It.IsAny<ProductVM>()).Result.Data;
+            int expected = 101;
+            mockDLRepo.Setup(svc => svc.AddProduct(product)).ReturnsAsync(expected);
+            int result = blRepo.AddProduct(product).Result.Data;
 
+            mockDLRepo.Verify(svc => svc.AddProduct(product));
             Assert.AreEqual(expected, result);
         }
 
         [Test]
         public void GetAllProducts_Test()
         {
-            List<Product>? response = new List<Product>()
+            List<Product>? products = new List<Product>()
             {
                 new Product()
                 {
@@ -56,45 +62,30 @@ namespace NC_BLRepositories.Tests
                 }
             };
 
-            mockDLRepo.Setup(svc => svc.GetAllProducts()).ReturnsAsync(response);
-            blRepo = new BLProductsRepo(mockDLRepo.Object);
-            List<ProductVM>? expected = response.Select(res => new ProductVM(res)).ToList();
+            mockDLRepo.Setup(svc => svc.GetAllProducts()).ReturnsAsync(products);
+            List<ProductVM>? expected = products.Select(res => new ProductVM(res)).ToList();
             List<ProductVM>? result = blRepo.GetAllProducts().Result.DataList;
 
             Func<ProductVM, object> selector = o => new { o.Id, o.Name };
             Assert.That(result.Select(selector), Is.EquivalentTo(expected.Select(selector)));
+            mockDLRepo.Verify(svc => svc.GetAllProducts());
         }
 
         [Test]
-        public void GetProductsById_Test()
+        public void GetProduct_Test()
         {
-            ProductVM expected = new ProductVM()
-            {
-                Id = 1,
-                Name = "Prod 1",
-                Price = 25.50m,
-                Status = 1
-            };
+            product.Id = 101;
+            mockDLRepo.Setup(svc => svc.GetProduct(product.Id)).ReturnsAsync(product);
+            ProductVM? result = blRepo.GetProduct(product.Id).Result.Data;
 
-            mockDLRepo.Setup(svc => svc.GetProduct(It.IsAny<int>())).ReturnsAsync(expected);
-            blRepo = new BLProductsRepo(mockDLRepo.Object);
-            ProductVM? result = blRepo.GetProduct(expected.Id).Result.Data;
-
-            Assert.AreEqual(expected.Id, result.Id);
-            Assert.AreEqual(expected.Name, result.Name);
+            Assert.AreEqual(product.Name, result.Name);
+            Assert.AreEqual(product.Price, result.Price);
+            mockDLRepo.Verify(svc => svc.GetProduct(product.Id));
         }
 
         [Test]
         public void EditProduct_Test()
         {
-            ProductVM request = new ProductVM()
-            {
-                Id = 1,
-                Name = "Prod 1",
-                Price = 25.50m,
-                Status = 1
-            };
-
             ProductVM expected = new ProductVM()
             {
                 Id = 1,
@@ -104,12 +95,34 @@ namespace NC_BLRepositories.Tests
             };
 
             mockDLRepo.Setup(svc => svc.EditProduct(expected)).ReturnsAsync(1);
-            blRepo = new BLProductsRepo(mockDLRepo.Object);
             int resultId = blRepo.EditProduct(expected).Result.Data;
+            mockDLRepo.Verify(svc => svc.EditProduct(expected));
+
+            mockDLRepo.Setup(svc => svc.GetProduct(expected.Id)).ReturnsAsync(expected);
             ProductVM result = blRepo.GetProduct(expected.Id).Result.Data;
 
-            Assert.AreEqual(result.Id, request.Id);
-            Assert.AreNotEqual(result.Name, request.Name);
+            Assert.AreEqual(result.Id, expected.Id);
+            Assert.AreEqual(result.Name, expected.Name);
+            Assert.AreEqual(result.Price, expected.Price);
+            Assert.AreNotEqual(result.Name, product.Name);
+            Assert.AreNotEqual(result.Price, product.Price);
         }
+
+        [Test]
+        public void DeleteProduct_Test()
+        {
+            product.Id = 101;
+            mockDLRepo.Setup(svc => svc.DeleteProduct(product.Id)).ReturnsAsync(1);
+            int resultId = blRepo.DeleteProduct(product.Id).Result.Data;
+            mockDLRepo.Verify(svc => svc.DeleteProduct(product.Id));
+
+            product = new ProductVM();
+            mockDLRepo.Setup(svc => svc.GetProduct(product.Id)).ReturnsAsync(product);
+            ProductVM? result = blRepo.GetProduct(product.Id).Result.Data;
+
+            Assert.AreEqual(result.Name, product.Name);
+            Assert.AreEqual(result.Price, product.Price);
+        }
+
     }
 }
